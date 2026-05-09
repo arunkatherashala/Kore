9aa0d280b2f46ad81ee4620f74f43830e8be089f
Sai Arun <arunkatherashala@gmail.com>
2026-05-09 18:27:56 -0400

    feat: Add Java source code, GPG signing, and workflow for Maven Central deployment

---
+++ language-bindings/java/src/main/java/io/kore/KoreFileformat.java
@@ -0,0 +1,24 @@
+package io.kore;
+
+/**
+ * KORE Binary Format - Java Bindings
+ * Killer Optimized Record Exchange format for efficient data serialization
+ */
+public class KoreFileformat {
+    
+    /**
+     * Get the version of the KORE library
+     * @return version string
+     */
+    public static String getVersion() {
+        return "0.1.0";
+    }
+    
+    /**
+     * Get the library name
+     * @return library name
+     */
+    public static String getName() {
+        return "KORE Fileformat";
+    }
+}
